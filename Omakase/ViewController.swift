0962fc6729e247d54311b0a9a269bd5fcab6655a
dishanest <hanzheng2222@gmail.com>
2020-12-27 01:19:59 -0600

    Create Xcode project

---
+++ Omakase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Omakase
+//
+//  Created by Hanzheng Li on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
